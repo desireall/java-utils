8bc20dacce18d2c5d65b0694495302c9e048d434
desireall <yu_huo_y@163.com>
2017-03-30 16:46:40 +0800

    httputil

--- src/main/java/pers/yuhuo/utils/http/HttpUtil.java
+++ src/main/java/pers/yuhuo/utils/http/HttpUtil.java
@@ -1,282 +1,186 @@
 package pers.yuhuo.utils.http;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.security.cert.X509Certificate;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-import java.util.Properties;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
+import org.apache.http.conn.ssl.DefaultHostnameVerifier;
+import org.apache.http.conn.util.PublicSuffixMatcher;
+import org.apache.http.conn.util.PublicSuffixMatcherLoader;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 /**
- * 工具类。
+ * 工具类 CloseableHttpClient 同步访问
  */
 public class HttpUtil {
 
-	// 系统错误
-	public static final int SYS_ERROR = -1000;
-
-	public static final int OK = 0;
-
-	private static ExecutorService pool = Executors.newFixedThreadPool(10);
-
-	public static void doAsynPost(final String url, final String body,
-			boolean isHttps) {
-		doAsynPost(url, null, body, isHttps);
-	}
-
-	public static void doAsynPost(final String url,
-			final Map<String, String> header, final String body,
-			final boolean isHttps) {
-		pool.execute(new Runnable() {
-
-			public void run() {
-				HttpUtil.doPost(url, null, body, isHttps);
-			}
-		});
-	}
-
-	public static void doAsynPost(final String url, final String body,
-			final HttpCallback callback, boolean isHttps) {
-		doAsynPost(url, null, body, callback, isHttps);
-	}
-
-	public static void doAsynPost(final String url,
-			final Map<String, String> header, final String body,
-			final HttpCallback callback, final boolean isHttps) {
-		pool.execute(new Runnable() {
-
-			public void run() {
-				String response = HttpUtil.doPost(url, header, body, isHttps);
-				callback.callback(response);
-			}
-		});
-	}
-
-	public static void doAsynPost(Runnable runnable) {
-		pool.execute(runnable);
-	}
+	private static final RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(15000)
+			.setConnectTimeout(15000).setConnectionRequestTimeout(15000).build();
 
 	/**
-	 * 执行一个HTTP POST请求，返回请求响应的内容
+	 * 发送 get请求
 	 * 
-	 * @param url
-	 *            请求的URL地址
-	 * @param params
-	 *            请求的查询参数,可以为null
-	 * @return 返回请求响应的内容
+	 * @param httpUrl
 	 */
-	public static String doPost(String url, String body, boolean isHttps) {
-		return doPost(url, null, body, isHttps);
-	}
-
-	public static String doPost(String url, Map<String, String> header,
-			String body, boolean isHttps) {
-		StringBuffer stringBuffer = new StringBuffer();
-		HttpEntity entity = null;
-		BufferedReader in = null;
-		HttpResponse response = null;
-		try {
-			HttpClient httpclient = new DefaultHttpClient();
-			if (isHttps) {
-				httpclient = wrapClient(httpclient);
-			}
-			HttpParams params = httpclient.getParams();
-			HttpConnectionParams.setConnectionTimeout(params, 20000);
-			HttpConnectionParams.setSoTimeout(params, 20000);
-			HttpPost httppost = new HttpPost(url);
-			httppost.setHeader("Content-Type",
-					"application/x-www-form-urlencoded");
-			if (header != null) {
-				for (String key : header.keySet()) {
-					httppost.setHeader(key, header.get(key));
-				}
-			}
-			if (body != null) {
-				httppost.setEntity(new ByteArrayEntity(body.getBytes("UTF-8")));
-			}
-			response = httpclient.execute(httppost);
-			entity = response.getEntity();
-			in = new BufferedReader(new InputStreamReader(entity.getContent()));
-			String ln;
-			while ((ln = in.readLine()) != null) {
-				stringBuffer.append(ln);
-				stringBuffer.append("\r\n");
-			}
-			httpclient.getConnectionManager().shutdown();
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (IllegalStateException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (null != in) {
-				try {
-					in.close();
-					in = null;
-				} catch (IOException e3) {
-					e3.printStackTrace();
-				}
-			}
-		}
-		return stringBuffer.toString();
-	}
-
-	/**
-	 * 读取配置信息。
-	 * 
-	 * @param key
-	 *            Key值
-	 * @return
-	 * @throws Exception
-	 */
-	public static String getConfig(String key) throws Exception {
-		try {
-			InputStream in = HttpUtil.class
-					.getResourceAsStream("/conf.Properties");
-			Properties p = new Properties();
-			p.load(in);
-			return p.get(key).toString().trim();
-		} catch (Exception e) {
-			e.printStackTrace();
-			// System.out.println("配置文件不存在" + e.toString());
-		}
-		return "";
-	}
-
-	/**
-	 * HTTPS
-	 * 
-	 * @param base
-	 * @return
-	 */
-	@SuppressWarnings("deprecation")
-	private static org.apache.http.client.HttpClient wrapClient(
-			org.apache.http.client.HttpClient base) {
-		try {
-			SSLContext ctx = SSLContext.getInstance("TLS");
-			X509TrustManager tm = new X509TrustManager() {
-
-				public void checkClientTrusted(X509Certificate[] xcs,
-						String string) {
-				}
-
-				public void checkServerTrusted(X509Certificate[] xcs,
-						String string) {
-				}
-
-				public X509Certificate[] getAcceptedIssuers() {
-					return null;
-				}
-			};
-			ctx.init(null, new TrustManager[] { tm }, null);
-			SSLSocketFactory ssf = new SSLSocketFactory(ctx);
-			ssf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-			ClientConnectionManager ccm = base.getConnectionManager();
-			SchemeRegistry sr = ccm.getSchemeRegistry();
-			sr.register(new Scheme("https", ssf, 443));
-			return new DefaultHttpClient(ccm, base.getParams());
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			return null;
-		}
-	}
-
-	public static String doGet(String url, Map<String, String> paramMap,
-			boolean https) {
+	public static String doGet(String url, Map<String, String> paramMap, boolean https) {
 		StringBuffer sb = new StringBuffer();
 		sb.append(url);
 		// 拼接URL
 		if (paramMap != null && paramMap.size() > 0) {
 			sb.append("?");
 			for (String key : paramMap.keySet()) {
-				sb.append(key).append("=").append(paramMap.get(key))
-						.append("&");
+				sb.append(key).append("=").append(paramMap.get(key)).append("&");
 			}
 			url = sb.substring(0, sb.length() - 1);
 		}
+		HttpGet httpGet = new HttpGet(url);// 创建get请求
+		return sendHttpGet(httpGet, https);
+	}
 
-		StringBuffer stringBuffer = new StringBuffer();
-		HttpEntity entity = null;
-		BufferedReader in = null;
-		HttpResponse response = null;
+	/**
+	 * 发送 post请求
+	 * 
+	 * @param httpUrl
+	 *            地址
+	 * @param params
+	 *            参数
+	 */
+	public static String doPost(String httpUrl, String params, boolean https) {
+		HttpPost httpPost = new HttpPost(httpUrl);// 创建httpPost
 		try {
+			// 设置参数
+			StringEntity stringEntity = new StringEntity(params, "UTF-8");
+			stringEntity.setContentType("application/json");
+			httpPost.setEntity(stringEntity);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return sendHttpPost(httpPost, https);
+	}
 
-			HttpClient httpclient = new DefaultHttpClient();
+	/**
+	 * 发送 post请求
+	 * 
+	 * @param httpUrl
+	 *            地址
+	 * @param maps
+	 *            参数
+	 */
+	public static String doPostWithParam(String httpUrl, Map<String, String> maps, boolean https) {
+		HttpPost httpPost = new HttpPost(httpUrl);// 创建httpPost
+		// 创建参数队列
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+		for (String key : maps.keySet()) {
+			nameValuePairs.add(new BasicNameValuePair(key, maps.get(key)));
+		}
+		try {
+			httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return sendHttpPost(httpPost, https);
+	}
+
+	/**
+	 * 发送Get请求
+	 * 
+	 * @param httpPost
+	 * @return
+	 */
+	private static String sendHttpGet(HttpGet httpGet, boolean https) {
+		CloseableHttpClient httpClient = null;
+		CloseableHttpResponse response = null;
+		HttpEntity entity = null;
+		String responseContent = null;
+		try {
+			// 创建默认的httpClient实例.
 			if (https) {
-				httpclient = wrapClient(httpclient);
+				PublicSuffixMatcher publicSuffixMatcher = PublicSuffixMatcherLoader
+						.load(new URL(httpGet.getURI().toString()));
+				DefaultHostnameVerifier hostnameVerifier = new DefaultHostnameVerifier(publicSuffixMatcher);
+				httpClient = HttpClients.custom().setSSLHostnameVerifier(hostnameVerifier).build();
+			} else {
+				httpClient = HttpClients.createDefault();
 			}
-			HttpParams params = httpclient.getParams();
-			HttpConnectionParams.setConnectionTimeout(params, 20000);
-			HttpConnectionParams.setSoTimeout(params, 20000);
-			// 去掉最后一个&
-			HttpGet httpGet = new HttpGet(url);
-			httpGet.setHeader("Content-Type",
-					"application/x-www-form-urlencoded; charset=utf-8");
 
-			response = httpclient.execute(httpGet);
+			httpGet.setConfig(requestConfig);
+			// 执行请求
+			response = httpClient.execute(httpGet);
 			entity = response.getEntity();
-			in = new BufferedReader(new InputStreamReader(entity.getContent()));
-			String ln;
-			while ((ln = in.readLine()) != null) {
-				stringBuffer.append(ln);
-				stringBuffer.append("\r\n");
-			}
-			httpclient.getConnectionManager().shutdown();
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (IllegalStateException e) {
-			e.printStackTrace();
+			responseContent = EntityUtils.toString(entity, "UTF-8");
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if (null != in) {
-				try {
-					in.close();
-					in = null;
-				} catch (IOException e3) {
-					 e3.printStackTrace();
+			try {
+				// 关闭连接,释放资源
+				if (response != null) {
+					response.close();
 				}
+				if (httpClient != null) {
+					httpClient.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
-		return stringBuffer.toString();
+		return responseContent;
 	}
 
-	public static interface HttpCallback {
+	/**
+	 * 发送Post请求
+	 * 
+	 * @param httpPost
+	 * @return
+	 */
+	private static String sendHttpPost(HttpPost httpPost, boolean https) {
+		CloseableHttpClient httpClient = null;
+		CloseableHttpResponse response = null;
+		HttpEntity entity = null;
+		String responseContent = null;
+		try {
+			// 创建默认的httpClient实例.
+			if (https) {
+				PublicSuffixMatcher publicSuffixMatcher = PublicSuffixMatcherLoader
+						.load(new URL(httpPost.getURI().toString()));
+				DefaultHostnameVerifier hostnameVerifier = new DefaultHostnameVerifier(publicSuffixMatcher);
+				httpClient = HttpClients.custom().setSSLHostnameVerifier(hostnameVerifier).build();
+			} else {
+				httpClient = HttpClients.createDefault();
+			}
+			httpPost.setConfig(requestConfig);
+			// 执行请求
+			response = httpClient.execute(httpPost);
+			entity = response.getEntity();
+			responseContent = EntityUtils.toString(entity, "UTF-8");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				// 关闭连接,释放资源
+				if (response != null) {
+					response.close();
+				}
+				if (httpClient != null) {
+					httpClient.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return responseContent;
+	}
 
-		public void callback(String response);
-	}
-  
-    public static void main(String[] args) {
-    	String url = "http://106.2.49.117:4019/sauth";
-    	
-    	System.err.println(doGet(url, null, false));
-    	
-	}
 }
